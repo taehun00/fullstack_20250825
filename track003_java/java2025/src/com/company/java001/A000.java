60bdcb14984e8e3d40db890e43193d99e8c0a5ae
Tae Hun <iiss07007@gmail.com>
2025-08-26 17:44:47 +0900

    day 002

---
+++ track003_java/java2025/src/com/company/java001/A000.java
@@ -0,0 +1,13 @@
+package com.company.java001; // 1. 부품의 위치
+
+// 주석, 설명
+public class A000 { // 2. 어디에서든지 접근, 부품 객체, 부품이름(클래스이름)
+	public static void main(String[] args) {
+// 3. 어디에서든지 접근(public), 바로사용가능 메모리(static), 
+//		전원버튼 이름(void main(String[] args)
+		System.out.println("I'm Java :) ");
+//		4. 운영체제부품객체, 출력(cmd), 출력(println) 
+	}
+} // end class
+
+//
---
+++ track003_java/java2025/src/com/company/java001_ex/A000_ex.java
@@ -0,0 +1,8 @@
+package com.company.java001_ex;
+
+public class A000_ex {
+	public static void main(String[] args) {
+		System.out.println("한 번 작성하면 어디서든 실행된다.");
+	}
+
+}
